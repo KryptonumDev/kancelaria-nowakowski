12064a510730e3e7c84a45c6b20f1e0b769a8447
kryptonumstudio <kryptonumstudio@gmail.com>
2023-03-21 14:55:54 +0100

    specialisation page graphql

--- src/templates/specializacja.jsx
+++ src/templates/specializacja.jsx
@@ -2,16 +2,62 @@ import { graphql } from "gatsby"
 import * as React from "react"
 
 export default function Specializacja({ data }) {
-    return (
-        <>
-        </>
-    )
+  return (
+    <>
+    </>
+  )
 }
 
 export const query = graphql`
-    query specializacja ($id: String!) {
-        wpPage(id: {eq: $id}){
-            id
+  query specializacja ($id: String!) {
+      global : wpPage(id: {eq: "cG9zdDoxNjg="}) {
+        global {
+          sectionContact {
+            sectionTitle
+            contentUnderTitle
+          }
+          sectionBlog {
+            sectionTitle
+            leftTextUnderTitle
+            rightTextUnderTitle
+            callToAction {
+              leftTitle
+              leftLink {
+                target
+                title
+                url
+              }
+              rightTitle
+              rightLink {
+                target
+                title
+                url
+              }
+            }
+          }
         }
-    }
+      }
+      wpPage(id: {eq: $id}){
+          specjalizacje {
+            sectionGridSpecialisation {
+              pageTitle
+              linksToSubpages {
+                link {
+                  target
+                  title
+                  url
+                }
+                backgroundImage {
+                  altText
+                  localFile {
+                    childImageSharp {
+                      gatsbyImageData
+                    }
+                  }
+                }
+              }
+            }
+          }
+      }
+  }
 `
