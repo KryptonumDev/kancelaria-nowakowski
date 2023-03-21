2eafe397f17d5cc2c36bf1cc496997aed19354cd
Bogumił <contact.bogumil@gmail.com>
2023-03-21 14:26:46 +0100

    added Hero to Homepage

--- src/components/organisms/Nav.js
+++ src/components/organisms/Nav.js
@@ -1,18 +1,245 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Link } from "gatsby";
 import styled from "styled-components";
+import { Logo } from '../atoms/Icons';
 
 const Nav = () => {
+  const nav = useRef();
+  const handleToggle = () => {
+    nav.current.classList.toggle('expand');
+  }
+
   return (
-    <StyledNav className="nav">
+    <StyledNav className="nav" ref={nav}>
       <div className="max-width">
-        
+        <Link to="/">
+          <Logo />
+        </Link>
+        <div className="nav-links">
+          <ul>
+            <li className="dropdown">
+              <Link to="/specjalizacje">Specjalizacje</Link>
+              <ul>
+                <li><Link to="/specjalizacje">Hałas lotniczy</Link></li>
+                <li><Link to="/specjalizacje">Kredyty CHF</Link></li>
+                <li><Link to="/specjalizacje">Odszkodowania</Link></li>
+                <li><Link to="/specjalizacje">Prawo spadkowe</Link></li>
+                <li><Link to="/specjalizacje">Proces sądowy</Link></li>
+                <li><Link to="/specjalizacje">Rozwód</Link></li>
+                <li><Link to="/specjalizacje">Utrata wartości</Link></li>
+              </ul>
+            </li>
+            <li>
+              <Link to="/kancelaria">Kancelaria</Link>
+            </li>
+            <li>
+              <Link to="/wspolpraca">Współpraca</Link>
+            </li>
+            <li>
+              <Link to="/blog">Blog</Link>
+            </li>
+          </ul>
+          <Link to="/kontakt" className="navLinks-cta">Kontakt</Link>
+        </div>
+        <Link to="/kontakt" className="nav-cta">Kontakt</Link>
+        <button
+          id="nav-toggle"
+          aria-label="Pokaż/Ukryj nawigację mobilną"
+          onClick={handleToggle}
+        >
+          <span></span><span></span><span></span>
+        </button>
       </div>
     </StyledNav>
   );
 }
 
 const StyledNav = styled.nav`
-
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  background-color: var(--neutral-200);
+  height: 105px;
+  z-index: 9;
+  .max-width {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    ::after {
+      content: '';
+      width: 100%;
+      height: 1px;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      background-color: var(--secondary-200);
+      z-index: -1;
+    }
+  }
+  ul {
+    list-style-type: none;
+    font-size: ${20/16}rem;
+  }
+  .nav-links {
+    > ul {
+      display: flex;
+      margin: 0 -${20/16}rem;
+      > li {
+        margin: 0 ${20/16}rem;
+        ul {
+          display: none;
+          width: 100%;
+          position: absolute;
+          padding: 2rem 0;
+          > li {
+            margin: 1rem 0;
+          }
+        }
+      }
+      a[aria-current="page"] {
+        font-weight: 700;
+        font-style: italic;
+      }
+    }
+  }
+  .nav-cta {
+    border: 2px solid var(--primary-800);
+    font-family: var(--serif);
+    padding: ${10/24}em ${32/24}em;
+    font-size: ${24/16}rem;
+  }
+  #nav-toggle {
+    display: none;
+    width: 48px;
+    height: 48px;
+    padding: 0 9px;
+    span {
+      display: block;
+      width: 100%;
+      height: 2px;
+      background-color: var(--primary-900);
+      transition: transform 0.3s ease 0s;
+      &:nth-child(odd){
+        width: 80%;
+      }
+      &:not(:last-child){
+        margin-bottom: 10px;
+      }
+    }
+  }
+  @media (max-width: 1149px){
+    height: 89px;
+    ul {
+      list-style-type: none;
+      font-size: ${22/16}rem;
+      font-family: var(--serif);
+    }
+    &.expand .nav-links {
+      transform: translateX(0);
+      visibility: visible;
+    }
+    .nav-links {
+      transform: translateX(100%);
+      visibility: hidden;
+      transition: transform .5s cubic-bezier(0.65,0.05,0.36,1), visibility .4s;
+      overflow-y: auto;
+      position: absolute;
+      right: 0;
+      top: 100%;
+      height: calc(100vh - 89px);
+      height: calc(100dvh - 89px);
+      padding: ${66/16}rem;
+      background-color: var(--neutral-400);
+      margin: 0 -20px;
+      > ul {
+        margin: 0;
+        flex-direction: column;
+        > li {
+          &:first-child {
+            order: 1;
+          }
+          margin: ${14/16}rem 0;
+          ul {
+            & > li {
+              font-family: var(--sans-serif);
+              font-size: ${20/16}rem;
+            }
+            padding: 0;
+            position: static;
+          }
+        }
+        a[aria-current="page"] {
+          font-weight: 700;
+          font-style: italic;
+        }
+      }
+    }
+    .nav-cta {
+      margin-left: auto;
+      margin-right: 34px;
+    }
+    #nav-toggle {
+      display: block;
+    }
+  }
+  .navLinks-cta {
+    display: none;
+  }
+  @media (max-width: 699px){
+    .nav-links {
+      width: calc(100% + 34px);
+      height: calc(100vh - 89px);
+      height: calc(100dvh - 89px);
+      padding: ${66/16}rem;
+      background-color: var(--neutral-200);
+      > ul {
+        margin: 0;
+        display: grid;
+        grid-template:"b a"
+                      "c a"
+                      "d a";
+        > li:nth-child(1){
+          grid-area: a;
+        }
+        > li:nth-child(2){
+          grid-area: b;
+        }
+        > li:nth-child(3){
+          grid-area: c;
+        }
+        > li:nth-child(4){
+          grid-area: d;
+        }
+        > li {
+          margin: ${14/16}rem 0;
+          ul {
+            & > li {
+              font-family: var(--sans-serif);
+              font-size: ${20/16}rem;
+            }
+            padding: 0;
+            position: static;
+          }
+        }
+        a[aria-current="page"] {
+          font-weight: 700;
+          font-style: italic;
+        }
+      }
+    }
+    .nav-cta {
+      display: none;
+    }
+    .navLinks-cta {
+      display: block;
+      border: 2px solid var(--primary-800);
+      font-family: var(--serif);
+      padding: ${10/24}em ${32/24}em;
+      font-size: ${24/16}rem;
+    }
+  }
 `
 
 export default Nav;
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -8,7 +8,36 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
   :root {
+    --primary-100: #E3F5F2;
+    --primary-200: #C6EBE4;
+    --primary-300: #AAE0D7;
+    --primary-400: #8DD6C9;
+    --primary-500: #71CCBC;
+    --primary-600: #5CA699;
+    --primary-700: #468176;
+    --primary-800: #315B54;
+    --primary-900: #1B3631;
+    --secondary-100: #FCF4E8;
+    --secondary-200: #EAF0F1;
+    --secondary-300: #F5DDBA;
+    --secondary-400: #F1D2A3;
+    --secondary-500: #EEC78C;
+    --secondary-600: #CDA972;
+    --secondary-700: #AC8A58;
+    --secondary-800: #8A6C3F;
+    --secondary-900: #694D25;
+    --neutral-100: #FDFDFC;
+    --neutral-200: #FBFBF9;
+    --neutral-300: #FAF9F6;
+    --neutral-400: #F8F7F3;
+    --neutral-500: #F6F5F0;
+    --neutral-600: #DDDACE;
+    --neutral-700: #C3BFAC;
+    --neutral-800: #AAA58B;
+    --neutral-900: #9D977A;
 
+    --sans-serif: 'Lato', sans-serif;
+    --serif: 'Literata', serif;
   }
   body {
     font-size: 16px;
@@ -17,6 +46,11 @@ const GlobalStyle = createGlobalStyle`
     overflow-x: hidden;
     min-width: 320px;
     font-family: 'Lato', sans-serif;
+    color: var(--primary-800);
+    margin-top: 105px;
+    @media (max-width: 1149px) {
+      margin-top: 89px;
+    }
   }
   button, select {
     cursor: pointer;
@@ -36,11 +70,38 @@ const GlobalStyle = createGlobalStyle`
   }
   h1, h2, h3, h4, h5, h6 {
     font-family: 'Literata', serif;
+    font-weight: 400;
+    color: var(--primary-900);
+    line-height: 1.22;
   }
   .max-width {
-    width: calc(100% - 34px);
+    width: calc(100% - 80px);
+    @media (max-width: 1366px){
+      width: calc(100% - 40px);
+    }
+    @media (max-width: 768px){
+      width: calc(100% - 34px);
+    }
     max-width: 1580px;
     margin: 0 auto;
+    height: 100%;
+  }
+  .cta-primary,
+  .cta-secondary {
+    font-family: var(--serif);
+    font-size: clamp(${18/16}rem, ${24/13.66}vw, ${24/16}rem);
+    padding: ${16/24}em ${32/24}em;
+    white-space: nowrap;
+    display: inline-block;
+    text-align: center;
+  }
+  .cta-primary {
+    background-color: var(--primary-500);
+    color: var(--primary-900);
+  }
+  .cta-secondary {
+    box-shadow: inset 0 0 0 2px var(--primary-800);
+    color: var(--primary-800);
   }
 `
 
