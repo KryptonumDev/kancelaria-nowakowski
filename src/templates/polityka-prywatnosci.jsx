7f535fa954e5f913c96a551175cbb53ee6a62acd
kryptonumstudio <kryptonumstudio@gmail.com>
2023-03-21 15:15:31 +0100

    privacy policy graphql

--- src/templates/polityka-prywatnosci.jsx
+++ src/templates/polityka-prywatnosci.jsx
@@ -11,7 +11,8 @@ export default function PolityaPrywatnosci({ data }) {
 export const query = graphql`
     query polityka ($id: String!) {
         wpPage(id: {eq: $id}){
-            id
+            title
+            content
         }
     }
 `
