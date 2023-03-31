c8ad333ec76e93b8715093217e1d7906fb87a7b9
kryptonumstudio <kryptonumstudio@gmail.com>
2023-03-31 17:48:45 +0200

    form submit

--- src/components/sections/ContactUs.js
+++ src/components/sections/ContactUs.js
@@ -1,85 +1,36 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { InputBorder, Ornament, RightArrow, SelectDropdown } from "../atoms/Icons";
 import { Link } from "gatsby";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
-let didSubmit = false;
 const ContactUs = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    tel: '',
-    type: '',
-    message: '',
-    legal: false,
-    address: '',
-  });
-  const [formError, setFormError] = useState({
-    name: '',
-    email: '',
-    tel: '',
-    type: '',
-    message: '',
-    legal: '',
-    error: '',
-  })
-  const [formSent, setFormSent] = useState(false);
-  const [formIsSending, setFormIsSending] = useState(false);
+  const { register, handleSubmit, reset, watch, formState: { errors } } = useForm();
+  const [sended, setSended] = useState(false)
 
-  const validateData = useCallback(() => {
-    let errors = {
-      name: formData.name.trim().length === 0 ? 'Pole wymagane' : '',
-      email:formData.email.trim().length === 0
-            ? ''
-            : (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(formData.email.toLowerCase())
-            ? ''
-            : 'Nieprawidłowy adres e-mail',
-      tel: (/^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{3,6}$/im).test(formData.tel.replaceAll(' ', '')) ? '' : 'Nieprawidłowy numer telefonu',
-      type: formData.type.length === 0 ? 'Pole wymagane' : '',
-      message: formData.message.trim().length === 0 ? 'Pole wymagane' : '',
-      legal: formData.legal === false ? 'Zgoda jest wymagana' : ''
-    }
-    setFormError(errors);
-    return errors;
-  }, [formData]);
+  const onSubmit = data => {
 
-  const handleSubmit = (e) => {
-    didSubmit = true;
-    e.preventDefault();
-    const validate = validateData();
-    let isValidate = true;
-    Object.keys(validate).forEach(key => validate[key] && (isValidate = false));
-    if(isValidate){
-      setFormIsSending(true);
-      fetch(e.target.action, {
-        method: 'POST', 
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(formData)
-      })
-      .then(response => response.json())
-      .then(response => {
-        setFormIsSending(false);
-        if(response.success){
-          document.cookie = `formSent=true;max-age=60;path=/`;
-          setFormSent(true);
+    let url = 'https://kancelaria.headlesshub.com/wp-json/contact-form-7/v1/contact-forms/13/feedback'
+
+    let body = new FormData()
+
+    body.append('email', data.email)
+    body.append("phone", data.tel)
+    body.append('fullname', data.name)
+    body.append('subject', data.subject)
+    body.append('date', data.date)
+
+    axios.post(url, body)
+      .then((res) => {
+        if (res.status === 200) {
+          setSended(true)
+          reset()
         } else {
-          setFormError(prevState => ({...prevState, error: 'Błąd podczas wysyłania formularza, spróbuj ponownie później.'}))
+          // toast('There was some problem with contact form, try later')
         }
       })
-      .catch(() => {
-        setFormIsSending(false);
-        setFormError(prevState => ({...prevState, error: 'Błąd podczas wysyłania formularza, spróbuj ponownie później.'}))
-      })
-    }
   }
-
-  useEffect(() => {
-    if(didSubmit){
-      validateData();
-    }
-  }, [formData, validateData])
 
   return (
     <Wrapper>
@@ -102,20 +53,15 @@ const ContactUs = () => {
         </ul>
       </div>
       <div className="form">
-        <form action="" onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <h3>Dane kontaktowe</h3>
           <div className="form-input">
             <label>
               <span>Imię i nazwisko</span>
-              <span className="error">{formError.name}</span>
               <div className="input">
                 <input
                   type="text"
-                  name="name"
-                  onChange={e => setFormData({
-                    ...formData,
-                    [e.target.name]: e.target.value
-                  })}
+                  {...register("name")}
                 />
                 <InputBorder />
               </div>
@@ -124,19 +70,10 @@ const ContactUs = () => {
           <div className="form-input">
             <label>
               <span>Adres e-mail</span>
-              {formError.email ? (
-                <span className="error">{formError.email}</span>
-              ) : (
-                <span>(opcjonalne)</span>
-              )}
               <div className="input">
                 <input
                   type="email"
-                  name="email"
-                  onChange={e => setFormData({
-                    ...formData,
-                    [e.target.name]: e.target.value
-                  })}
+                  {...register("email")}
                 />
                 <InputBorder />
               </div>
@@ -145,15 +82,10 @@ const ContactUs = () => {
           <div className="form-input">
             <label>
               <span>Telefon</span>
-              <span className="error">{formError.tel}</span>
               <div className="input">
                 <input
                   type="tel"
-                  name="tel"
-                  onChange={e => setFormData({
-                    ...formData,
-                    [e.target.name]: e.target.value
-                  })}
+                  {...register("tel")}
                 />
                 <InputBorder />
               </div>
@@ -164,7 +96,7 @@ const ContactUs = () => {
             <label>
               <span>Wybierz temat:</span>
               <div className="input">
-                <select name="subject">
+                <select {...register("subject")}>
                   <option value="odszkodowania">Odszkodowania</option>
                   <option value="odszkodowania">Odszkodowania</option>
                   <option value="odszkodowania">Odszkodowania</option>
@@ -179,14 +111,14 @@ const ContactUs = () => {
             <label>
               <span>Wybierz datę i godzinę</span>
               <div className="input">
-                <input type="datetime-local" name="date" />
+                <input min={new Date().toISOString().slice(0,new Date().toISOString().lastIndexOf(":"))} type="datetime-local" {...register("date")} />
                 <InputBorder />
               </div>
             </label>
           </div>
           <div className="form-input">
             <label>
-              <input type="checkbox" name="legal" />
+              <input type="checkbox"  {...register("legal")} />
               <span>Wyrażam zgodę na przetwarzanie moich danych osobowych na zasadach określonych w <Link to="/polityka-prywatnosci">Polityce prywatności</Link></span>
             </label>
           </div>
@@ -204,26 +136,26 @@ const Wrapper = styled.section`
   display: grid;
   grid-template-areas: "header form" "content form";
   grid-template-columns: 1.2fr 1fr;
-  column-gap: ${140/13.66}vw;
+  column-gap: ${140 / 13.66}vw;
   .header {
     grid-area: header;
     h2 {
-      margin-top: ${32/16}rem;
+      margin-top: ${32 / 16}rem;
     }
   }
   .content {
     grid-area: content;
     p {
-      margin: ${48/16}rem 0 ${24/16}rem;
+      margin: ${48 / 16}rem 0 ${24 / 16}rem;
       font-family: var(--serif);
-      font-size: clamp(${20/16}rem, ${24/13.66}vw, ${26/16}rem);
+      font-size: clamp(${20 / 16}rem, ${24 / 13.66}vw, ${26 / 16}rem);
       & + h3 {
         margin-bottom: 1rem;
       }
     }
     ul {
       list-style-type: none;
-      font-size: clamp(${21/16}rem, ${21/7.68}vw, ${24/16}rem);
+      font-size: clamp(${21 / 16}rem, ${21 / 7.68}vw, ${24 / 16}rem);
       font-family: var(--serif);
       li {
         position: relative;
@@ -246,10 +178,10 @@ const Wrapper = styled.section`
           background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='32' height='33' fill='none'%3E%3Cpath stroke='%230F3730' strokeLinecap='round' stroke-linejoin='round' strokeWidth='1.5' d='M19.2 13.282a3.2 3.2 0 1 0-6.399-.002 3.2 3.2 0 0 0 6.4.002Z' clip-rule='evenodd'/%3E%3Cpath stroke='%230F3730' strokeLinecap='round' strokeWidth='1.5' d='M16 29.814c0-4.804-9.364-8.958-9.6-16.568C6.235 7.902 10.697 3.147 16 3.147c5.302 0 9.763 4.755 9.6 10.099-.238 7.766-9.6 11.636-9.6 16.568Z' clip-rule='evenodd'/%3E%3C/svg%3E");
         }
         > ul {
-          margin-top: ${8/16}rem;
+          margin-top: ${8 / 16}rem;
           li {
             &:not(:last-child) {
-              margin-bottom: ${8/16}rem;
+              margin-bottom: ${8 / 16}rem;
             }
             &::before {
               background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='32' height='33' fill='none'%3E%3Cpath stroke='%230F3730' strokeLinecap='square' strokeWidth='1.5' d='M26.665 16.48H5.332M19.12 24.027c0-3.88 3.416-7.547 7.546-7.547M19.12 8.934c0 3.88 3.416 7.546 7.546 7.546'/%3E%3C/svg%3E");
@@ -262,8 +194,8 @@ const Wrapper = styled.section`
   .form  {
     grid-area: form;
     h3 {
-      font-size: ${24/16}rem;
-      margin: ${32/16}rem 0 ${12/16}rem 0;
+      font-size: ${24 / 16}rem;
+      margin: ${32 / 16}rem 0 ${12 / 16}rem 0;
       &:first-of-type {
         margin-top: 0;
       }
@@ -274,15 +206,15 @@ const Wrapper = styled.section`
       }
       width: 100%;
       &:not(:last-child) {
-        margin-bottom: ${12/16}rem;
+        margin-bottom: ${12 / 16}rem;
       }
       label {
         display: flex;
         justify-content: space-between;
         flex-wrap: wrap;
         span:first-child {
-          font-size: ${20/16}rem;
-          margin: 0 0 ${4/16}rem ${4/16}rem;
+          font-size: ${20 / 16}rem;
+          margin: 0 0 ${4 / 16}rem ${4 / 16}rem;
         }
         span:last-child {
           font-size: 1rem;
@@ -309,7 +241,7 @@ const Wrapper = styled.section`
           width: 100%;
           height: 100%;
           border: 2px solid var(--secondary-600);
-          padding: ${13/16}rem ${8/16}rem;
+          padding: ${13 / 16}rem ${8 / 16}rem;
           &:focus + svg .ornament {
             fill: #f90;
           }
@@ -338,7 +270,7 @@ const Wrapper = styled.section`
         }
         & + span {
           width: calc(100% - 32px);
-          font-size: ${14/16}rem;
+          font-size: ${14 / 16}rem;
           a {
             text-decoration: underline;
           }
@@ -349,7 +281,7 @@ const Wrapper = styled.section`
   @media (max-width: 768px){
     grid-template-areas: "header" "form" "content";  
     grid-template-columns: 1fr;
-    row-gap: ${42/16}rem;
+    row-gap: ${42 / 16}rem;
     .content {
       p {
         margin-top: 0;
@@ -357,5 +289,5 @@ const Wrapper = styled.section`
     }
   }
 `
- 
+
 export default ContactUs;
